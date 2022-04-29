0a38455b0b3c670252eb64e457341bd6b64c9a63
mataieb <matthieu.taieb@capgemini.com>
2022-04-29 18:20:26 +0200

    test

--- routes/queries.js
+++ routes/queries.js
@@ -7,8 +7,15 @@ const datafull = fs.readFileSync('./fullnftquery.sql').toString();
 const dataselector = fs.readFileSync('./selector.sql').toString();
 
 const Pool = require("pg").Pool;
+
+const config = {
+	connectionString: 'postgres://fqisttotgpivew:94a31ac293dd56ca6c5ea1cad4de724dc1ab22b1fa2fdacdb186b0c381df4c79@ec2-52-31-201-170.eu-west-1.compute.amazonaws.com:5432/d1m36gutvhjag1?sslmode=require',
+	ssl: {
+		rejectUnauthorized: false,
+	}
+}
 const pool = new Pool({
-	
+
 	// user: "postgres",
 	// host: "localhost",
 	// database: "test2",
@@ -19,15 +26,23 @@ const pool = new Pool({
 	host: "ec2-52-31-201-170.eu-west-1.compute.amazonaws.com",
 	database: "d1m36gutvhjag1",
 	password: "94a31ac293dd56ca6c5ea1cad4de724dc1ab22b1fa2fdacdb186b0c381df4c79",
-
 	port: 5432,
 });
+
+const pool2 = new Pool(config);
+pool2.connect().then(client => {
+	console.log('connected')
+    client.release()
+  })
+  .catch(err => console.error('error connecting', err.stack))
+  .then(() => pool.end())
 // Route pour afficher tous les NFT sur une même page, avec filtre via url sur les attributs background, aura, body etc.
 // http://<DNS>/Filter?<attribute1>=<value1>&<attribute2>=<value2>,<value3>
 
- const Filter = (req, res, next) => {	  
+ const Filter = (req, res, next) => {
+	 const nftid = (Boolean(req.query.id)) ? req.query.id : "*";
 	pool.query(
-		datafull,
+		datafull, [nftid],
 	(error, results) => {
 		if (error) {
 			throw error;
@@ -62,13 +77,13 @@ const nftpage = (req, res, next) => {
 			}
 			res.status(200).json(results.rows);
 		}
-	);	
+	);
 
 };
 
 // Route pour afficher la drop down list d'un sélecteur. Affichage de tous les attributs de la table sélectionnée
 //http://<DNS>/selector/<table_name>
-const selector = (req, res, next) => {	  
+const selector = (req, res, next) => {
 	var param = req.params.param;
 	var paramid = param + "id";
 
@@ -80,7 +95,7 @@ const selector = (req, res, next) => {
 			}
 			res.status(200).json(results.rows);
 		}
-	);	
+	);
 }
 
 
